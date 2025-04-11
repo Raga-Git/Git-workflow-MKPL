5040ddf03aca3039aef77319ac0a8f06c3bcd1da
Raga-Git <ragadhitya@gmail.com>
2025-04-11 18:57:41 +0700

    Menambahkan fungsi penjumlahan

---
+++ Tambah.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperasiSederhana
+{
+    internal class Tambah
+    {
+        public int operasiTambah(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
--- obj/Debug/net8.0/OperasiSederhana.AssemblyInfo.cs
+++ obj/Debug/net8.0/OperasiSederhana.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OperasiSederhana")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab4f5fe29c883851c5d7d296b15b17d56f730010")]
 [assembly: System.Reflection.AssemblyProductAttribute("OperasiSederhana")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OperasiSederhana")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
