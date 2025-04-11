27986a8f77a9f1c04cfcbf503feb222aa0e54867
Raga-Git <ragadhitya@gmail.com>
2025-04-11 19:15:28 +0700

    Menambahkan fitur pengurangan

---
+++ Kurang.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperasiSederhana
+{
+    internal class Kurang
+    {
+        public int operasiKurang(int x, int y)
+        {
+            return x -y;
+        }
+    }
+}
